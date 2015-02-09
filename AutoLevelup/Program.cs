861ad165283c6686e6986dafca3128373a8838c4
xkgary <kiyozxc@hotmail.com>
2015-02-10 00:04:57 +0800

    Morgana

--- AutoLevelup/Program.cs
+++ AutoLevelup/Program.cs
@@ -233,7 +233,7 @@ namespace AutoLevelup
                 if (ObjectManager.Player.Masteries.Where(mastery => mastery.Page == MasteryPage.Utility)
     .Any(mastery => mastery.Id == 100 && mastery.Points == 1))
                 {
-                    abilitySequence = new int[] { 1, 2, 3, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
+                    abilitySequence = new int[] { 1, 3, 2, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
                     tipo = " Support";
                 }
                 else
